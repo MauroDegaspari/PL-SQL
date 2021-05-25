5cdee75b3107c62b64de21029f9361f33e64ca43
maurodegaspari <mauro_degaspari@hotmail.com>
2021-05-24 23:46:51 -0300

    exercicio 5 DONE

--- ExercicioSql.sql
+++ ExercicioSql.sql
@@ -1,39 +1,43 @@
 -- MV SISTEMAS
 -- CURSO: SQL-
 -- PROGRAMA: MULTIPLIKA UMV
--- FACILITADOR: JOÃO FERRAZ
+-- FACILITADOR: JOÃƒO FERRAZ
 -- COLABORADOR: MAURO DEGASPARI
 -- EQUIPE: SUPRIMENTOS
--- DATA DE REFERÊNCIA: 05/2021
+-- DATA DE REFERÃŠNCIA: 05/2021
 
                                       -- DESAFIOS --
 
--- 1 – Criar uma View podendo usar sub-select para retornar as colunas Código do Usuário,
---  Código do produto, nome do produto, nome da espécie, utilizando a configuração de usuário por classificação de estoque
--- (dbamv.usuário_espécie). Lembrando que nessa tabela o código do usuário e a espécie são obrigatórios, mas se forem preenchidas as
---  colunas classe, sub-classe e produto a configuração fica mais restrita.
+-- 1 â€“ Criar uma View podendo usar sub-select para retornar as colunas CÃ³digo do UsuÃ¡rio,
+--  CÃ³digo do produto, nome do produto, nome da espÃ©cie, utilizando a configuraÃ§Ã£o de usuÃ¡rio por classificaÃ§Ã£o de estoque
+-- (dbamv.usuÃ¡rio_espÃ©cie). Lembrando que nessa tabela o cÃ³digo do usuÃ¡rio e a espÃ©cie sÃ£o obrigatÃ³rios, mas se forem preenchidas as
+--  colunas classe, sub-classe e produto a configuraÃ§Ã£o fica mais restrita.
 
             SELECT * FROM dbamv.usuarios_especie;                     CREATE OR REPLACE VIEW v_codigoUsuario  AS
             SELECT
             FROM
             WHERE;
 
---  2 – Criar uma tabela de cópia de produtos e criar um script de merge mantendo a cópia atualizada.
+--  2 â€“ Criar uma tabela de cÃ³pia de produtos e criar um script de merge mantendo a cÃ³pia atualizada.
 
-        CREATE TABLE t_produtoCopia (
-          cd_produto NUMBER,
-          ds_produto VARCHAR2(255),
-          qt_produto NUMBER,
-          dt_cadastro DATE
+        CREATE TABLE DBAMV.PRODUTO_COPIA (
+                                          cd_produto NUMBER,
+                                          ds_produto VARCHAR2(255),
+                                          qt_produto NUMBER,
+                                          dt_cadastro DATE
             );
 
+           
+
+            SELECT * FROM PRODUTO_COPIA;
+
             SELECT * FROM produto;
-            SELECT * FROM t_produtoCopia;
+          SELECT cd_produto, ds_produto 
+          FROM dbamv.produto
+          WHERE ds_produto LIKE 'CA%' ;
 
-            SELECT * FROM t_produtoCopia;
 
-
-         MERGE INTO dbamv.t_produtoCopia copia
+         MERGE INTO DBAMV.PRODUTO_COPIA copia
           USING  dbamv.produto original
           ON     (copia.cd_produto = original.cd_produto)
           WHEN MATCHED THEN
@@ -49,62 +53,69 @@
 
 
 
--- 3 – Criar uma consulta de saldo (dbamv.est_pro) de produtos (dbamv.produto) da espécie medicamentos, que não estão na tabela
---     de item de prescrição (dbamv.tip_presc). Deve exibir nessa lista o código e nome do estoque, código e nome do produto, o código
---     e o nome do mestre na mesma linha, a unidade padrão de acordo com a função e o saldo atual.
---     Para os produtos que não tem mestre informar na descrição o texto ‘MESTRE NÃO INFORMADO’.
-
+-- 3 â€“ Criar uma consulta de saldo (dbamv.est_pro) de produtos (dbamv.produto) da espÃ©cie medicamentos, que nÃ£o estÃ£o na tabela
+--     de item de prescriÃ§Ã£o (dbamv.tip_presc). Deve exibir nessa lista o cÃ³digo e nome do estoque, cÃ³digo e nome do produto, o cÃ³digo
+--     e o nome do mestre na mesma linha, a unidade padrÃ£o de acordo com a funÃ§Ã£o e o saldo atual.
+--     Para os produtos que nÃ£o tem mestre informar na descriÃ§Ã£o o texto â€˜MESTRE NÃƒO INFORMADOâ€™.
+        SELECT * FROM dbamv.est_pro;
+        SELECT * FROM dbamv.produto;
+        SELECT * FROM dbamv.tip_presc;
+              
         SELECT e.cd_estoque,p.cd_produto, p.ds_produto
         FROM dbamv.est_pro e,
-             dbamv.produto p;
-        WHERE
+             dbamv.produto p
+        WHERE sn_mestre ='S';
 
             NOT IN
             NOT EXISTS
 
-        SELECT * FROM dbamv.tip_presc;
-
-        SELECT e.cd_estoque, p.cd_produto, p.ds_produto
-        FROM dbamv.est_pro e, dbamv.produto p, dbamv.tip_presc t
-        WHERE;
+             
 
 
 
--- 4 – Inserir uma nova fábrica na tabela DBAMV.FABRICA, inserir novos carros, inserir todos
---     acessórios já existentes para os novos carros usando um sub-select.
+-- 4 â€“ Inserir uma nova fÃ¡brica na tabela DBAMV.FABRICA, inserir novos carros, inserir todos
+--     acessÃ³rios jÃ¡ existentes para os novos carros usando um sub-select.
 
-            CREATE TABLE DBAMV.FABRICA(CD_FABRICA     NUMBER(12) NOT NULL,
-                                       NM_FABRICA     VARCHAR2(100) NOT NULL,
+--AULA VIDEO SQL 1 20210405
+
+            CREATE TABLE DBAMV.FABRICA(CD_FABRICA     NUMBER NOT NULL,
+                                       NM_FABRICA     VARCHAR2(100),
                                        DT_CADASTRO    DATE DEFAULT SYSDATE NOT NULL,
                                        DT_DESATIVACAO DATE,
-                                       SN_ATIVA       VARCHAR2(1) DEFAULT 'S' NOT NULL );0
+                                       SN_ATIVA       VARCHAR2(1) DEFAULT 'S' NOT NULL );
+
+            SELECT * FROM ALL_CONSTRAINTS WHERE TABLE_NAME LIKE 'FABRICA';
+
 
             SELECT * FROM dbamv.fabrica;
 
 
--- 5 -  Crie uma consulta na ultima consolidação (dbamv.c_conest) , somando valor final de estoque, agrupando por estoque,
---     e mostrando o percentual do saldo final de cada estoque. O único filtro da consulta deve ser o Código da empresa.
+-- 5 -  Crie uma consulta na ultima consolidaÃ§Ã£o (dbamv.c_conest) , somando valor final de estoque, agrupando por estoque,
+--     e mostrando o percentual do saldo final de cada estoque. O Ãºnico filtro da consulta deve ser o CÃ³digo da empresa.
 
-  SELECT * FROM dbamv.c_conest
-   WHERE cd_ano = 2021
-    AND cd_mes = 4
-   ORDER BY cd_ano DESC, cd_mes desc;
-
-
--- 6 – Criar a atualização da coluna do saldo da tabela dbam.lot_pro usando a função dbamv.fnc_mges_saldo_anterior_lote (essa função retorna
---      o saldo calulado do lote, pra o saldo atual user com parametro a função de data e hora atual). O filtro deve ser
---      o estoque e produto. Lembrando que a função retorna o saldo na unidade padrão e a lot_pro guarda o saldo na unidade de referencia.
+            SELECT cd_produto,
+                  (vl_estoque_final+vl_estoque_final) vl_final,
+                  Round(vl_estoque_final+vl_estoque_final)/vl_estoque_final*100 vl_percentual
+            FROM dbamv.c_conest
+            WHERE cd_ano = 2021
+            AND cd_mes = 4
+            ORDER BY cd_ano DESC, cd_mes desc;
 
 
--- 7 – Faça uma consulta usando como parametro a função sysdate e utilizando as funções de data retorne o primeiro dia do ano, primeiro dia
---     do ano seguinte,  primeiro dia do mês, ultimo dia do mês, quantidade de dias do ano até a data atual.
+-- 6 â€“ Criar a atualizaÃ§Ã£o da coluna do saldo da tabela dbam.lot_pro usando a funÃ§Ã£o dbamv.fnc_mges_saldo_anterior_lote (essa funÃ§Ã£o retorna
+--      o saldo calulado do lote, pra o saldo atual user com parametro a funÃ§Ã£o de data e hora atual). O filtro deve ser
+--      o estoque e produto. Lembrando que a funÃ§Ã£o retorna o saldo na unidade padrÃ£o e a lot_pro guarda o saldo na unidade de referencia.
 
--- 8 – Crie uma consulta para retirar descrição do produto os textos (MESTRE), (M) e MESTRE, para produtos que não são mestres.
 
--- 9 – Crie uma consulta que torne a descrição do produto, o tipo (sn_mestre – retornar a palavra NORMAL, MESTRE ou OUTROS)
---     e a quantidade de repetições. Só retornar os produto que tem mais de uma ocorrência, ou seja os produtos duplicados.
+-- 7 â€“ FaÃ§a uma consulta usando como parametro a funÃ§Ã£o sysdate e utilizando as funÃ§Ãµes de data retorne o primeiro dia do ano, primeiro dia
+--     do ano seguinte,  primeiro dia do mÃªs, ultimo dia do mÃªs, quantidade de dias do ano atÃ© a data atual.
+
+-- 8 â€“ Crie uma consulta para retirar descriÃ§Ã£o do produto os textos (MESTRE), (M) e MESTRE, para produtos que nÃ£o sÃ£o mestres.
+
+-- 9 â€“ Crie uma consulta que torne a descriÃ§Ã£o do produto, o tipo (sn_mestre â€“ retornar a palavra NORMAL, MESTRE ou OUTROS)
+--     e a quantidade de repetiÃ§Ãµes. SÃ³ retornar os produto que tem mais de uma ocorrÃªncia, ou seja os produtos duplicados.
 SELECT * FROM
 dbamv.produto;
 
--- 10 -  Coloque no nosso grupo do treinamento no Teams um trecho de código novo que você aprendeu ou que achou mais
+-- 10 -  Coloque no nosso grupo do treinamento no Teams um trecho de cÃ³digo novo que vocÃª aprendeu ou que achou mais
 --       interessante durante o curso.
